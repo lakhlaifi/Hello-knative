dd5c30aec84bbc69def8d65310d7bfccf5998e2c
lakhlaifi <youssef.lakhlaifi18@gmail.com>
2021-03-08 14:13:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import flask,requests
 
 app = flask.Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
-def home():
-    data = {"msg": "Service OK"}
-    return data
+@app.route('/<country_code>/<zipcode>', methods=['GET'])
+def home(country_code, zipcode):
+    Api="http://api.zippopotam.us/"+country_code+"/"+zipcode
+    response = requests.get(Api)    
+    return response.json()
 
 app.run(port=8080)
